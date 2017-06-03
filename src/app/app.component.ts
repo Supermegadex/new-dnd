d16b4be5ca139f6457c6fff9b3358d40b5f80afe
Daniel Noon <supercookies@outlook.com>
2017-06-03 11:54:24 -0500

    Character card minimization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,20 @@ import { MdTabGroup } from "@angular/material";
   providers: [MdTabGroup]
 })
 export class AppComponent {
-  character; showChar; races = Array<Race>(); points = 6;
+  character; showChar; races = Array<Race>(); points = 6; minmax = 0;
+  static getItemByProperty(query, property, arr) {
+    let outcome;
+    //noinspection TsLint
+    for (let i in arr) {
+      if (arr[i][property] === query) {
+        outcome = arr[i];
+      }
+    }
+    return outcome;
+  }
   constructor(public tabs: MdTabGroup) {
     console.log(tabs);
-    if(!localStorage.getItem("karmaRoll")) {
+    if (!localStorage.getItem("karmaRoll")) {
       localStorage.setItem("karmaRoll", String(Math.floor(Math.random() * 6)));
     }
     this.character = {
@@ -48,16 +58,6 @@ export class AppComponent {
     return this.character.stats[stat] + this.character.mod[stat];
   }
 
-  static getItemByProperty(query, property, arr) {
-    let outcome;
-    for (let i in arr) {
-      if (arr[i][property] === query) {
-        outcome = arr[i];
-      }
-    }
-    return outcome;
-  }
-
   addPoint(stat) {
     if (this.points > 0) {
       this.character.mod[stat]++;
@@ -66,26 +66,57 @@ export class AppComponent {
   }
 
   subtractPoint(stat) {
-    if (this.points < 6) {
+    if (this.points < 6 && this.character.mod[stat] > 0) {
       this.character.mod[stat]--;
       this.points++
     }
   }
 
   pickRace(name) {
-    let race = AppComponent.getItemByProperty(name, "name", this.races);
+    const race = AppComponent.getItemByProperty(name, "name", this.races);
     this.character.stats = race.stats;
     this.character.racial = race.racial;
     this.character.img = race.img;
     this.character.race = race.name;
     this.character.karma = race.karma(localStorage.getItem("karmaRoll"));
     this.showChar = true;
-    let nameTab:any = document.querySelector(".mat-tab-label:nth-child(2)");
+    const nameTab: any = document.querySelector(".mat-tab-label:nth-child(2)");
     nameTab.click();
+  }
+  getMinMaxClass(el) {
+    if (el === "card") {
+      switch (this.minmax) {
+        case 0:
+          return "";
+        case 1:
+          return "character-card-max";
+        case 2:
+          return "character-card-min";
+      }
+    }
+    if (el === "button") {
+      switch (this.minmax) {
+        case 0:
+          return "";
+        case 1:
+          return "character-button-max";
+        case 2:
+          return "character-button-min";
+      }
+    }
+  }
+  toggleCardState() {
+    this.minmax === 2 ? this.maximize() : this.minimize();
+  }
+  maximize() {
+    this.minmax = 1;
+  }
+  minimize() {
+    this.minmax = 2;
   }
 
   chooseName() {
-    let statsTab:any = document.querySelector(".mat-tab-label:nth-child(3)");
+    const statsTab: any = document.querySelector(".mat-tab-label:nth-child(3)");
     statsTab.click();
   }
 }
